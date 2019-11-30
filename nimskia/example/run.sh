a7a405abb270f5c474f508cf269fdd124b774287
mvenditto <matteo.venditto@studio.unibo.it>
2019-11-30 15:03:21 +0100

    changed examples compile path

--- nimskia/example/run.sh
+++ nimskia/example/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 bin="$(cut -d'.' -f1 <<< "$1")"
-nim c "$1" && LD_LIBRARY_PATH=../../skia/out/Shared/:../libs/ "./$bin"
+nim c -o:./bin/ "$1" && LD_LIBRARY_PATH=../../skia/out/Shared/:../libs/ "./bin/$bin"
