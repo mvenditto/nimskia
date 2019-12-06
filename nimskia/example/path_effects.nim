e4402498f99d5ba7335293e1e717a61cea75c6a3
mvenditto <matteo.venditto@studio.unibo.it>
2019-12-06 22:12:07 +0100

    updated path effects sample

--- nimskia/example/path_effects.nim
+++ nimskia/example/path_effects.nim
@@ -18,22 +18,31 @@ const
 
 proc main() =
 
-  let dash = newPath().addOval(newRectF(0,0,16,6), Clockwise)
+  let dash = newPath().addOval(newRectF(0,0,8,3), Clockwise)
+  let oval = newPath().addOval(newRectF(50,100,430,380), Clockwise)
+  let effect1 = sum(
+        newDescretePathEffect(10.0, 5.0, 795755684),
+        newDescretePathEffect(20.0, 40.0, 795755684)
+    )
+  let effect2 = new1DPathEffect(dash, 20, 0, Rotate)
+
 
   let strokePaint = newPaint()
   strokePaint.style = Stroke
-  strokePaint.color = Teal
   strokePaint.strokeWidth = 2
   strokePaint.strokeCap = RoundCap
   strokePaint.antialias = true
-  strokePaint.pathEffect =  sum(
-        newDescretePathEffect(10.0, 5.0, 795755684),
-        newDescretePathEffect(20.0, 50.0, 795755684)
-    )
   
   proc update(canvas: SKCanvas, dt: float) =
     canvas.clear(White)
-    canvas.drawCircle(240, 240, 122,strokePaint)
+
+    strokePaint.color = Teal
+    strokePaint.pathEffect =  effect1
+    canvas.drawCircle(240, 240, 100,strokePaint)
+
+    strokePaint.color = DarkRed
+    strokePaint.pathEffect =  effect2
+    canvas.drawPath(oval, strokePaint)
 
   let sample = Sample(
     title: title,
